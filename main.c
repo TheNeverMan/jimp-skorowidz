94b2076dec074efa5df792ad46eae5f0d590470b
TheNeverMan <thenevermanever@gmail.com>
2024-11-29 15:43:47 +0000

    feat: poprawny main z obsługą błędów

--- main.c
+++ main.c
@@ -4,8 +4,17 @@
 
 int main(int argv, char** argc)
 {
-    
-    if(argv > 1)
-
-    return 0;
+  Stack* dataFromFile = NULL;
+  if(argv == 1)
+  {
+    puts("Brak podanych argumentów!");
+    return -1;
+  }
+  dataFromFile = readFile(argc[1]);
+  if(!dataFromFile)
+  {
+    puts("Nie udało się otworzyć pliku lub plik nie istnieje!");
+    return -1;
+  }
+  return 0;
 }
