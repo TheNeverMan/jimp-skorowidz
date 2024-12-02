65ce94416128f1a89bfd50821b2d9b58b8935331
TheNeverMan <thenevermanever@gmail.com>
2024-12-02 17:29:59 +0000

    docs: added comments to all stack functions

--- data/stack.h
+++ data/stack.h
@@ -18,8 +18,20 @@ typedef struct Stack
 
 typedef int bool;
 
+
 Stack* createNewEmptyStack();
+
+/* puts element at the end of the stack, data is pointer to element
+which will be inserted */
 void pushToStack(Stack* stack, void* data);
+
+/* removes element from top of the stack and returns it, if stack is empty returns
+NULL. You are responsible for freeing the element
+*/
 void* popFromStack(Stack* stack);
+
+/*returns number of elements on stack*/
 size_t getStackLength(Stack* stack);
+
+/* returns true if given stack is empty*/
 bool isStackEmpty(Stack* stack);
